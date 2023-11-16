7e6317a6c9d63b2eb751a2de5a92fe5e656e9c25
Baran Akhavan - Yekta <200529109@student.georgianc.on.ca>
2023-11-15 20:52:18 -0500

    testing

--- 23F-JavaScript-03-Week11-main/js/json.js
+++ 23F-JavaScript-03-Week11-main/js/json.js
@@ -2,44 +2,47 @@
 const header = document.querySelector("header");
 const section = document.querySelector("section");
 
-const baseUrl = "https://baranyekta.github.io/23F-JavaScript-03-Week11-main/";
-
 // STEP 3a: Create the asynchronous function populate()
 async function populate() {
-    const url = baseUrl + "js/i-scream.json";
-
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4a: Create i-scream.json file with companyName, headOffice, established, active, topFlavors(name, calories, type, ingredients, image) */
     // STEP 4b: Store the URL of a JSON file in a variable */
 
-    //const url = "https://baranyekta.github.io/23F-JavaScript-03-Week11-main/js/i-scream.json";
+    const url = "https://priyansht.github.io/23F-JavaScript-03-Week11/js/i-scream.json";
 
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(url);
+
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
     const response = await fetch(request);
+
     // STEP 7: Capture the returned Response object and covert to a JSON object using json()
     const responseJson = await response.json();
+
     // STEP 8: Output the iScream JSON object to the console
     console.log(responseJson);
+
     // STEP 9a: Invoke the populateHeader function here, then build it below
     populateHeader(responseJson);
+
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
     showTopFlavors(responseJson);
 }
 
 // STEP 3b: Call the populate() function
-//    populate();
+    populate();
 
 /* STEP 9b: Build out the populateHeader() function */
 function populateHeader(jsonData) {
     // Create the H1 element
     const h1 = document.createElement("h1"); // <h1></h1>
     const p = document.createElement("p");
+
     // Grab the company name from the JSON object and use it for the text node
     h1.textContent = jsonData.companyName; // <h1>I-Scream Company Inc.</h1>
     p.textContent = `${jsonData.headOffice} est. ${jsonData.established} - 
     ${(jsonData.active) ? "Active" : "Inactive"}`;
+
     // Inject the complete H1 element into the DOM, inside the HEADER
     header.appendChild(h1); // <header> <h1>I-Scream Company Inc.</h1> </header>
     header.appendChild(p);
@@ -64,7 +67,7 @@ function showTopFlavors(jsonData) {
         p1.textContent = `Calories: ${topFlavors[i].calories}`;
         p2.textContent = `Type: ${topFlavors[i].type}`;
         //image.setAttribute("src", topFlavors[i].image) // <img src="image-location">
-        image.setAttribute("src", baseUrl + topFlavors[i].image); // Update the image URL
+        image.setAttribute("src", topFlavors[i].image) // <img src="image-location">
 
         // STEP 10g: Build a loop for the ingredients array in the JSON
         const ingredients = topFlavors[i].ingredients;
@@ -74,10 +77,6 @@ function showTopFlavors(jsonData) {
             listItem.textContent = ingredients[j];
             list.appendChild(listItem); // // <ul><li></li></ul>
         }
-
-        const url = baseUrl + "js/i-scream.json";
-
-        populate();
 
         // STEP 10i: Append each complete ARTICLE element to the SECTION element
         article.appendChild(h2);
